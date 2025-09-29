c8e1add5b029fcd388bbd4bfa913e3dabcec5dd9
riyazmohammad4020-ai <riyazmohammad4020@gmail.com>
2025-09-29 11:43:04 +0530

    Update perimeter_rectangle.c

--- perimeter_rectangle.c
+++ perimeter_rectangle.c
@@ -1,26 +1,15 @@
 #include <stdio.h>
-
-/*
-   Variables to store the width and height of a rectangle in inches
-*/
 int width;
 int height;
-
-int area;           /* Variable to store the area of the rectangle */
-int perimeter;      /* Variable to store the perimeter of the rectangle */
-
+int area;          
+int perimeter;
 int main() {
-    /* Assigning values to height and width */
-	height = 7;
-	width = 5;
-
-    /* Calculating the perimeter of the rectangle */
-    perimeter = 2*(height + width);
+	height = 9;
+	width = 7;
+    perimeter = 5*(height + width);
 	printf("Perimeter of the rectangle = %d inches\n", perimeter);
-
-    /* Calculating the area of the rectangle */
 	area = height * width;
 	printf("Area of the rectangle = %d square inches\n", area);
-
-    return(0);
+     return(0);
 }
+
